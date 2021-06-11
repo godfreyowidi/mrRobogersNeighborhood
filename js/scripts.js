235c4e42b50c796aaf9c7983514c5154df912dc5
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-11 20:55:59 +0300

    add function to check numbers 4-9.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
-function alertNeighbor(number) {
-  if (number === 0) {
-  return ("" + number);
+function numbersLessThan4(number) {
+  if (number === 0) { 
+  return ("" + number)
 } else {
   if (number === 1) {
   return 1;
 } else {
-  if (number === 2) {
+  if (number === 2) { 
     return 1;
 } else {
-  if (number === 3) {
+  if (number === 3) { 
     return 1;
+} else {
+  return 0;
+}
+
+function numbers4To9(number) {
+  const fourToNine = [4, 5, 6, 7, 8, 9];
+  fourToNine.forEach(function(element) {
+    if (element.includes(number[0]-1))
   }
 }
