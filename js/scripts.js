9d17803c051a0878e908fe5c51c6a33db9ed641e
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-11 20:28:00 +0300

    add function to handle numbers 1-3.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function alertNeighbor(number) {
+  if (number === 0) {
+  return ("" + number);
+} else {
+  if (number === 1) {
+  return 1;
+} else {
+  if (number === 2) {
+    return 1;
+} else {
+  if (number === 3) {
+    return 1;
+  }
+}
