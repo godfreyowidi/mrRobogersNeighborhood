493adaf83edb695e4aa6e451c31bc034dd4becb1
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-11 23:03:30 +0300

    add revision with to fourToNine function with for loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,54 @@
-function numbersLessThan4(number) {
+// global vaiables
+const zero = "0";
+const one = "Beep";
+const two = "Boop";
+const three = "won't you be my neighbor?";
+const four = "'0', 'Beep', 'Boop', 'won't you be my neighbor', 4";
+
+function numbersLessThan5(number) {
   if (number === 0) { 
-  return ("" + number)
+  return zero;
 } else {
   if (number === 1) {
-  return 1;
+  return one;
 } else {
   if (number === 2) { 
-    return 1;
+    return two;
 } else {
   if (number === 3) { 
-    return 1;
+    return three;
+} else {
+  if (number === 4) {
 } else {
   return 0;
 }
 
+
+
 function numbers4To9(number) {
-  const fourToNine = [4, 5, 6, 7, 8, 9];
-  fourToNine.forEach(function(element) {
-    if (element.includes(number[0]-1))
+  let fiveToNine = [];
+  if (number === 5) {
+    for (let i = 5; i != 6; ++i) 
+    return four + fiveToNine.push(i)
+  } else if (number === 6) {
+    for (let i = 5; i != 7; ++i) 
+    return four + fiveToNine.push(i)
+  } else if (number === 7) {
+    for (let i = 5; i != 8; ++i) 
+    return four + fiveToNine.push(i)
+  } else if (number === 8) {
+    for (let i = 5; i != 9; ++i) 
+    return four + fiveToNine.push(i)
+  } else if (number === 9) {
+    for (let i = 5; i != 10; ++i) 
+    return four + fiveToNine.push(i)
   }
 }
+
+function numbersMorethan9(number) {
+  for (let i=0; i <= number.length; i++);
+    if (i === 3);{
+    return true;
+  } else {
+    if (i === )
+  }
