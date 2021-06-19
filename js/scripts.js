1e48d96ebb6c31f81729a769b4045e2448a69feb
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-19 22:42:29 +0300

    add map() method for array of numbers to input value.

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,22 @@
-
-  // vaiables
-  const zero = "0";
-  const one = "Beep";
-  const two = "Boop";
-  const three = "won't you be my neighbor?";
-  const four = "'0', 'Beep', 'Boop', 'won't you be my neighbor', 4";
-
   // numbers less than 5
-  function numbersLessThan5(number) {
-    if (number == 0) {
-      return zero;
-    }
-    else if (number == 1) {
-      return one;
-    }
-    else if (number == 2) {
-      return two;
-    }
-    else if (number == 3) {
-      return three;
-    }
-    else if (number == 4) {
-      return four;
-    }
-  
-    // numbers from 5 - 9
-  function numberFrom5To9(number) {
-    let result = [];
-    for (let i = 5; i < number.length; ++i)
-    if (number.includes(5)) {
-      return(four + result.push[i]);
-    } else if (number.icludes(6)) {
-      return(four + result.push[i]);
-    } else if (number.includes(7)) {
-      return(four + result.push[i]);
-    } else if (number.icludes(8)) {
-      return(four + result.push[i]);
-    } else if (number.includes(9));
-      return(four + result.push[i]);
-  }
-    // numbers more than 9 (more than 1 character number)
-  function numberMoreThan9(number) {
-    for (let i = 0; i < number.length; i++);
-    if (number[i] == 3) {
-      return three;
-    }
-    else if (number[i] == 2 && number[i] !== 3) {
-      return two;
-    }
-    else if (number[i] == 1 && number[i] !== 2 && number[i] !== 3) {
-      return one;
-    }
-  }
-    // UI                
+  function beepBoop(number) {
+    const zero = "0";
+    const one = "Beep";
+    const two = "Boop";
+    const three = "won't you be my neighbor?";
+    
+    let numberList = Array.apply(null, {length: number}).map(Number.call, Number);
+      return(numberList);
+      arr.forEach(function(element) {
+        if (element === 0);
+        return zero;  }
+
+// UI                
 $(document).ready(function () {
   $("form#users").submit(function (event) {
     event.preventDefault();
-    const number = $("#integer").val();
-    const numbersInput = alertNeighbor(number);
+    const numberInput = $("#integer").val();
+    const numbersInput = numbersAlert(numberInput)
     $("#alert").html(numbersInput);
   });
 });
- 
-
