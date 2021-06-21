343cd6665b98a494b56e7bb89742bb71140d6c09
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-21 16:37:58 +0300

    add function with for loop to push the numbers as strings.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,11 @@
-  // numbers less than 5
-  function beepBoop(number) {
-    const zero = "0";
-    const one = "Beep";
-    const two = "Boop";
-    const three = "won't you be my neighbor?";
-    
-    let numberList = Array.apply(null, {length: number}).map(Number.call, Number);
-      return(numberList);
-      arr.forEach(function(element) {
-        if (element === 0);
-        return zero;  }
+const beep = "beep!"
+const boop = "boop!"
+const neighbor = "Won't you be my neighbor?"
 
-// UI                
-$(document).ready(function () {
-  $("form#users").submit(function (event) {
-    event.preventDefault();
-    const numberInput = $("#integer").val();
-    const numbersInput = numbersAlert(numberInput)
-    $("#alert").html(numbersInput);
-  });
-});
+
+function beepBoop(number) {
+  let numberList = [];
+  for (let i = 0; i <= number; i++) {
+    numberList.push(i.toString());
+  };
+
